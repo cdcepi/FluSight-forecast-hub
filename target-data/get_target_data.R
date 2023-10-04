e23ce14d0b32e88108d90d46dd2cf2814ef6acd9
smathis <66260252+smathis14@users.noreply.github.com>
2023-10-04 14:44:09 -0400

    set target data to update Friday

--- target-data/get_target_data.R
+++ target-data/get_target_data.R
@@ -4,7 +4,7 @@
 #' @param temporal_resolution "daily" or "weekly"
 #' @param na.rm boolean indicating whether NA values should be dropped when
 #'   aggregating state-level values and calculating weekly totals. Defaults to
-#'   `FALSE`
+#'   `TRUE`
 #'
 #' @return data frame of flu incidence with columns date, location,
 #'   location_name, value, weekly_ratee
@@ -14,10 +14,12 @@ fetch_flu <- function(temporal_resolution = "weekly", na.rm = TRUE){
   require(lubridate)
   require(RSocrata)
   
+  #read data from healthdata.gov, filtering for when flu reporting became mandatory
   health_data = RSocrata::read.socrata(url = "https://healthdata.gov/resource/g62h-syeh.json") %>% 
     dplyr::filter(date >= as.Date("2022-02-02"))
   
-  recent_data = health_data %>% filter(date >= as.Date("2022-02-02")) %>% 
+  #remove  VI and AS as they are not included for FluSight, keep only necessary vars and add epiweek and epiyear 
+  recent_data = health_data %>% 
     dplyr::filter(!state %in% c("VI", "AS")) %>% 
     dplyr::select(state, date, previous_day_admission_influenza_confirmed) %>% 
     dplyr::rename("value" = "previous_day_admission_influenza_confirmed") %>% 
@@ -26,14 +28,17 @@ fetch_flu <- function(temporal_resolution = "weekly", na.rm = TRUE){
                   epiweek = lubridate::epiweek(date), 
                   epiyear = lubridate::epiyear(date))
   
+  #summarize US Flu 
   us_data = recent_data %>% dplyr::group_by(date, epiweek, epiyear) %>% 
     dplyr::summarise(value = sum(value, na.rm = na.rm)) %>% 
     dplyr::mutate(state = "US") %>% 
     dplyr::ungroup()
   
+  #bind state and US data
   full_data = rbind(recent_data, us_data) %>% 
     dplyr::left_join(., locations, by = join_by("state" == "abbreviation"))
   
+  #convert counts to weekly and calculates weekly rate 
   weeklydat = full_data %>% 
     dplyr::group_by(state,epiweek,epiyear, location, location_name, population) %>% 
     dplyr::summarise(value = sum(value, na.rm = na.rm), date = max(date), num_days = n()) %>% 
@@ -42,6 +47,7 @@ fetch_flu <- function(temporal_resolution = "weekly", na.rm = TRUE){
     dplyr::select(-num_days, -epiweek, -epiyear) %>% 
     dplyr::mutate(weekly_rate = (value*100000)/population )
   
+  #if daily data is ever wanted, this returns correct final data
   if(temporal_resolution == "weekly"){
     final_dat = weeklydat %>% 
       dplyr::select(date, location, location_name, value, weekly_rate) %>% 
@@ -56,10 +62,10 @@ library(dplyr)
 library(lubridate)
 library(RSocrata)
 library(readr)
-locations <- read_csv(file = "https://raw.githubusercontent.com/cdcepi/FluSight-forecast-hub/main/auxiliary-data/locations.csv") %>% 
-  select(1:4)
+locations <- readr::read_csv(file = "https://raw.githubusercontent.com/cdcepi/FluSight-forecast-hub/main/auxiliary-data/locations.csv", col_select= 1:4)
 
+  
 target_data <- fetch_flu(temporal_resolution = "weekly")
 
-readr::write_csv(target_data, file = "./target-data/target-hospital-admissions.csv")
+readr::write_csv(target_data, file = "./target-data/target-hospital-admissions.csv", append = FALSE)
 readr::write_csv(target_data, file = paste0("./auxiliary-data/target-data-archive/target-hospital-admissions_", max(target_data$date),".csv"))
