1d61a70facd9400ab775762510e6784ee2996a7c
HershV123 <hershvo@umich.edu>
2023-10-11 19:09:34 -0400

    Finalized Oct 11

--- model-output/Validation.R
+++
@@ -1,6 +0,0 @@
-setwd("~/Desktop/FluSight-forecast-hub/")
-library(hubValidations)
-hubValidations::validate_submission(
-  hub_path=".",
-  file_path="UM-DeepOutbreak/2023-10-14-UM-DeepOutbreak.csv")
-
