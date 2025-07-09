f79fad17678aeba88e42fb7da63fdfbcabed6b6e
Annabella Hines <118386400+Annabella-Hines@users.noreply.github.com>
2025-07-09 12:43:47 -0400

    Update get_target_data.R

--- target-data/get_target_data.R
+++ target-data/get_target_data.R
@@ -7,11 +7,45 @@
 fetch_flu <- function(){
   require(dplyr)
   require(lubridate)
-  require(RSocrata)
+  require(jsonlite)
+  require(purrr)
+  #require(RSocrata)
   
   #read data from data.cdc.gov, filtering for when flu reporting became mandatory
-  health_data = RSocrata::read.socrata(url = "https://data.cdc.gov/resource/mpgq-jmmr.json") %>% 
-    dplyr::filter(weekendingdate >= as.Date("2022-02-01"))
+  # health_data = RSocrata::read.socrata(url = "https://data.cdc.gov/resource/mpgq-jmmr.json") %>% 
+  #   dplyr::filter(weekendingdate >= as.Date("2022-02-01"))
+  
+  # Function to fetch paginated data
+  fetch_socrata_data <- function(base_url, limit = 1000, max_rows = 20000) {
+    offsets <- seq(0, max_rows, by = limit)
+    
+    all_data <- map_dfr(offsets, function(offset) {
+      paged_url <- paste0(base_url, "?$limit=", limit, "&$offset=", offset)
+      message("Fetching offset: ", offset)
+      tryCatch(
+        fromJSON(paged_url),
+        error = function(e) {
+          message("Error at offset ", offset, ": ", e$message)
+          NULL
+        }
+      )
+    })
+    
+    return(all_data)
+  }
+  
+  # CDC Socrata endpoint
+  base_url <- "https://data.cdc.gov/resource/mpgq-jmmr.json"
+  
+  # Download all data (adjust `max_rows` if needed)
+  health_data_raw <- fetch_socrata_data(base_url, limit = 1000, max_rows = 100000)
+  
+  # Now filter it
+  health_data <- health_data_raw %>%
+    mutate(weekendingdate = as.Date(weekendingdate)) %>%
+    filter(weekendingdate >= as.Date("2022-02-01"))
+  
+  
   
   #remove  VI and AS as they are not included for FluSight, keep only necessary vars and add epiweek and epiyear 
   recent_data = health_data %>% 
@@ -36,7 +70,9 @@ fetch_flu <- function(){
 
 library(dplyr)
 library(lubridate)
-library(RSocrata)
+library(jsonlite)
+library(purrr)
+#library(RSocrata)
 
 locations <- read.csv(file = "https://raw.githubusercontent.com/cdcepi/FluSight-forecast-hub/main/auxiliary-data/locations.csv") %>% dplyr::select(1:4)
 
@@ -45,6 +81,3 @@ locations <- read.csv(file = "https://raw.githubusercontent.com/cdcepi/FluSight-
 target_data <- fetch_flu()
 
 write.csv(target_data, file = "./target-data/target-hospital-admissions.csv", row.names = FALSE)
-
-
-
